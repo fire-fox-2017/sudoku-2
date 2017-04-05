ac25d13e8913330da9e7190b2ad0930b5a64e966
oscarhermawan <franky1790@gmail.com>
2017-04-05 14:31:57 +0700

    done-sudoku-2

--- sudoku.js
+++ sudoku.js
@@ -1,160 +1,166 @@
-let parseBoard = function(board) {
-    let sudokuBoard = [];
-    let a=0;
-    for (let i = 0; i < 9; i++) {
-      let row = []
-      sudokuBoard.push(row)
-      for (let j = 0; j < 9; j++) {
-        row.push(Number(board[a]))
-        a++
+"use strict"
+
+class Sudoku {
+  constructor(board_string) {
+  this._value = board_string;
+  this._board = [];
+  }
+
+  board() {
+    let baris =[]
+    let k=0;
+    for(let i=0; i<9;i++){
+      baris =[];
+      for(let j=0;j<9;j++){
+          baris.push(this._value[k])
+          k++;
+      }
+      this._board.push(baris);
+    }
+    console.log(this._board);
+  }
+
+  cekValueTersisa(arrTerpakai){
+    let string;
+    let valueTersisa=[];
+    for(let i=1;i<=9;i++){
+        string=''+i;
+        if(arrTerpakai.indexOf(string) == -1 ){
+            valueTersisa.push(''+i)
+        }
+    }
+    return valueTersisa;
+  }
+
+
+  cekValueTerpakai(baris, kolom){
+    let tmpValue=[]
+
+    for(let i=0; i<9;i++){
+      for(let j=0;j<9;j++){
+          if(i==baris){
+              tmpValue.push(this._board[i][j])
+          }
+
+          if(j==kolom){
+              tmpValue.push(this._board[i][j])
+          }
       }
     }
-    return sudokuBoard;
-};
 
+    var columnCorner = 0,
+        rowCorner = 0,
+        squareSize = 3;
 
+    // Find the left-most column
+    while(kolom >= columnCorner + squareSize) {
+      columnCorner += squareSize;
+    }
 
+    // Find the upper-most row
+    while(baris >= rowCorner + squareSize) {
+      rowCorner += squareSize;
+    }
 
-let saveEmptyPositions = function(board) {
-  // Create an array to save the positions
-  var emptyPositions = [];
-
-  // Check every square in the puzzle for a zero
-  for(var i = 0; i < board.length; i++) {
-    for(var j = 0; j < board[i].length; j++) {
-      // If a zero is found, so that position
-      if(board[i][j] === 0) {
-        emptyPositions.push([i, j]);
+    // Iterate through each row
+    for(var i = rowCorner; i < rowCorner + squareSize; i++) {
+      // Iterate through each column
+      for(var j = columnCorner; j < columnCorner + squareSize; j++) {
+        // Return false is a match is found
+        tmpValue.push(this._board[i][j])
       }
     }
-  }
-  // Return the positions
-  return emptyPositions;
-};
-
- let checkRow = function(board, row, value) {
-  // Iterate through every value in the row
-  for(var i = 0; i < board[row].length; i++) {
-    // If a match is found, return false
-    if(board[row][i] === value) {
-      return false;
-    }
-  }
-  // If no match was found, return true
-  return true;
-};
-
-let checkColumn = function(board, column, value) {
-  // Iterate through each value in the column
-  for(var i = 0; i < board.length; i++) {
-    // If a match is found, return false
-    if(board[i][column] === value) {
-      return false;
-    }
-  }
-  // If no match was found, return true
-  return true;
-};
-
-let check3x3Square = function(board, column, row, value) {
-  // Save the upper left corner
-  var columnCorner = 0,
-      rowCorner = 0,
-      squareSize = 3;
-
-  // Find the left-most column
-  while(column >= columnCorner + squareSize) {
-    columnCorner += squareSize;
+    return tmpValue;
   }
 
-  // Find the upper-most row
-  while(row >= rowCorner + squareSize) {
-    rowCorner += squareSize;
-  }
-
-  // Iterate through each row
-  for(var i = rowCorner; i < rowCorner + squareSize; i++) {
-    // Iterate through each column
-    for(var j = columnCorner; j < columnCorner + squareSize; j++) {
-      // Return false is a match is found
-      if(board[i][j] === value) {
-        return false;
+  cekKordinatKosong(){
+    let arr=[]
+    for(let row=0; row<9 ; row++){
+      for(let col=0; col<9; col++){
+        if(this._board[row][col] == '0'){
+          arr.push([row, col])
+        }
       }
     }
+    return arr;
   }
-  // If no match was found, return true
-  return true;
-};
 
-let checkValue = function(board, column, row, value) {
-  if(checkRow(board, row, value) &&
-    checkColumn(board, column, value) &&
-    check3x3Square(board, column, row, value)) {
-    return true;
-  } else {
-    return false;
-  }
-};
 
-let solvePuzzle = function(board, emptyPositions) {
-  // Variables to track our position in the solver
-  var limit = 9, i, row, column, value, found;
-  for(i = 0; i < emptyPositions.length;) {
-    row = emptyPositions[i][0];
-    column = emptyPositions[i][1];
-    // Try the next value
-    value = board[row][column] + 1;
-    // Was a valid number found?
-    found = false;
-    // Keep trying new values until either the limit
-    // was reached or a valid value was found // dicapai atau nilai yang valid ditemukan
-    while(!found && value <= limit) {
-      // If a valid value is found, marked found to true,
-      // set the position to the value, and move to the
-      // next position
-      if(checkValue(board, column, row, value)) {
-        found = true;
-        board[row][column] = value;
-        i++;
+  solve() {
+    let kordinatKosong = this.cekKordinatKosong() //berbentuk array of array
+    let kordinatKosongTerakhir = kordinatKosong[kordinatKosong.length-1]
+    let i=0
+    let j=0
+    let z=0 // z hanya untuk uji coba
+    // console.log(this._board[kordinatKosong[i][0]][kordinatKosong[i][1]])
+    // let cek = this.cekValueTerpakai(kordinatKosong[1][0], kordinatKosong[1][1])
+    // console.log(kordinatKosong[0][0], kordinatKosong[0][1])
+    // let sisaValue = this.cekValueTersisa(cek)
+    // console.log(sisaValue)
+    do{
+      let cek = this.cekValueTerpakai(kordinatKosong[i][0], kordinatKosong[i][1])
+      let sisaValue = this.cekValueTersisa(cek)
+      //jika tidak ada nilai tersisa saat maju di set 0, dan saat mundur, tidak ada nilai juga
+      if(sisaValue.length == 0) {
+        // console.log('\nIF : ', i, sisaValue)
+        this._board[kordinatKosong[i][0]][kordinatKosong[i][1]] = '0'
+        i--
       }
-      // Otherwise, try the next value
-      else {
-        value++;
+
+
+      else if(sisaValue.length>0){
+          // console.log('\nELSE : ', i, sisaValue)
+          if(this._board[kordinatKosong[i][0]][kordinatKosong[i][1]]!= '0'){
+            let valuesebelumnya = this._board[kordinatKosong[i][0]][kordinatKosong[i][1]]
+            // console.log('ELSE value sebelumnya : ', valuesebelumnya)
+            this._board[kordinatKosong[i][0]][kordinatKosong[i][1]] = '0'
+            cek = this.cekValueTerpakai(kordinatKosong[i][0], kordinatKosong[i][1])
+            sisaValue = this.cekValueTersisa(cek)
+            for(let i=0; i<sisaValue.length; i++){
+              if(sisaValue[i] == valuesebelumnya){
+                var k=i;
+              }
+            }
+            j=k+1;
+            // console.log('indeksnya : ',j)
+            // console.log('sisaValue length', sisaValue.length)
+          }
+
+          if(j>sisaValue.length-1){
+            this._board[kordinatKosong[i][0]][kordinatKosong[i][1]] = '0'
+            i--
+            j=0
+          }
+          else{
+            this._board[kordinatKosong[i][0]][kordinatKosong[i][1]] = sisaValue[j]
+            i++;
+            j=0
+          }
       }
-    }
-    // If no valid value was found and the limit was
-    // reached, move back to the previous position
-    // Jika tidak ada nilai yang valid ditemukan dan batas itu
-    //  mencapai, kembali ke posisi sebelumnya
-    if(!found) { // false
-      board[row][column] = 0;
-      i--;
-    }
+      z++
+    // }while(z<40)
+    }while(this._board[kordinatKosongTerakhir[0]][kordinatKosongTerakhir[1]] =='0')
+    console.log(''+z+' Percobaan')
+    console.log(this._board)
   }
 
-  // A solution was found! Log it // Sebuah solusi ditemukan! log itu
-  // board.forEach(function(row) {
-  //   console.log(row.join());
-  // });
 
-  // return the solution
-  return board;
-};
 
-function solveSudoku(board) {
-  var parsedBoard = parseBoard(board);
-  var emptyPositions = saveEmptyPositions(parsedBoard);
+}
 
-  console.log(parsedBoard)
-  return solvePuzzle(parsedBoard, emptyPositions);
-
-};
-
+// The file has newlines at the end of each line,
+// so we call split to remove it (\n)
 var fs = require('fs')
 var board_string = fs.readFileSync('set-04_peter-norvig_11-hardest-puzzles.txt')
   .toString()
-  .split("\n")[2]
+  .split("\n")[4]
+
+var game = new Sudoku(board_string)
 
 
-console.log('\n--------SOLVED--------')
-console.log(solveSudoku(board_string))
+game.board();
+console.log('-------')
+
+game.solve()
+
+
